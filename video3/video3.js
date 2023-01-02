38b7e928f73c54f9a09b0aeaeefa882720189911
JRabbit <ledinhtho686@gmail.com>
2023-01-02 21:48:17 +0700

    chapter 2 video 3

---
+++ video3/video3.js
@@ -0,0 +1,7 @@
+console.log('hello world form js')
+
+let x = 5, y = 6, z = 7;
+
+console.log("  Test string", 'x= ', x, ' y= ', y, ' z= ', z)
+
+console.log(" Test string 222 :", + 'x = ' + x + 'y= ' + y + 'z= ' + z)
